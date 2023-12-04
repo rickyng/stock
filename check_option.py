fd0ebe9f91cc60dcee18e173ed6cf2564caad4b6
ricky.ng <ricky.big@gmail.com>
2023-12-04 19:02:28 +0800

    Update

--- check_option.py
+++ check_option.py
@@ -1,66 +1,260 @@
 import yahoo_fin.options as ops
-import yahoo_fin.stock_info as si 
+import yahoo_fin.stock_info as si
 import pandas as pd
 from datetime import datetime as dt
 from datetime import timedelta
-#expiration_dates = ops.get_expiration_dates("aapl")
-#print(expiration_dates)
-#stocks =  ["msft"]
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
-class Stragegy:
+import matplotlib.dates as mdates
+
+# expiration_dates = ops.get_expiration_dates("aapl")
+# print(expiration_dates)
+# stocks =  ["msft"]
+
+class Strategy:
     def __init__(self):
-        self.balance = 0.0
+        pass
+
+class DynamicStrategy(Strategy):
+    def __init__(self, name, put_margin, call_margin, hold):
+        self.stock = 0.0
+        self.option = 0.0
         self.position = 0
         self.sp = None
         self.sc = None
-        self.assigned =[]
+        self.hold = hold
+        self.assigned = []
+        self.name = "Dynamic " + name + " Put " + str(put_margin) + "%" + " Call " + str(call_margin) + "% (min hold " + str(
+            hold) + ")"
+        self.num = 0
+        self.numSP = 0
+        self.numSC = 0
+        self.put_margin = put_margin
+        self.call_margin = call_margin
+        self.maxPostion = 0
+        self.max = None
+        self.min = None
+        self.positionList = []
+        self.valueList = []
+        self.scList = defaultdict(list)
 
-    def Check(self, date, open, close):
-        if self.sp != None:
-            if  self.sp > close: 
-                self.assigned.append( self.sp)
-                
-        for a in self.assigned:
-            if close > a:
-                self.assigned.remove(a)
-        if close - open > 0:
-            self.sp = close -1 - len(self.assigned)
-            self.balance += round(100 * 1/ (1+len(self.assigned)),2)
-        else:
-            self.sp = None
-        print (date, open, close, self.balance, self.assigned)
-        
+    def Check(self, row):
+        close = row["close"]
+        open = row["open"]
 
-actions = ["call", "put"]
-stocks =  ["qqq"]
-#stocks =  ["sq", "amd", "dis", "v"]
-for s in stocks:
-    print (s)
-    p = si.get_live_price(s)
-    data = si.get_data(s, start_date ="1/1/2020")
-    today = dt.strptime("26/7/2020", "%d/%m/%Y")
-    start = dt.strptime("6/1/2020", "%d/%m/%Y")
-    end = dt.strptime("10/1/2020", "%d/%m/%Y")
-    prev_close = None
-    sp = None
-    assigned = 0
-    s =  Stragegy()
-    while start < today :
-        range = data.loc[start : end]
-        #print (range)
-        high = round(range["high"].max(), 2)
-        open = round(range.iloc[0]["open"], 2)
-        close = round(range.iloc[-1]["close"], 2)
-        low = round(range["low"].min(), 2 )
-        #print ( "Up" if (open  < close) else "Down" )
-        upper = high - open
-        body =  close - open 
-        lower = close - low
-        
-        #print ("%s-%s, %.2f,%.2f, %.2f, %.2f"%(start.strftime("%d/%m/%Y"), end.strftime("%d/%m/%Y"), high, open, close, low))
-        
-        s.Check(start.strftime("%d/%m/%Y"), open,close)
-        start += timedelta(7)
-        end += timedelta(7)
-        
-        
+        if self.sp is not None:
+            if self.sp > close:
+                # print(self.name, "assingment buy", self.assigned, self.sp, close)
+                self.assigned.append(round(self.sp, 2))
+                self.position += 100
+                self.stock -= 100 * self.sp
+                # print ("Buy %f with %f"%(self.sp, close) )
+        if self.scList :
+
+            sold =[]
+            #print (type(self.scList))
+            total = 0
+            for k, v in self.scList.items():
+                total = total + len(v)
+                if k < close:
+                    for i in v:
+                        self.assigned.remove(i)
+                        self.position -= 100
+                        self.stock += 100 * k
+                        sold.append(i)
+            if (len(sold) > 1  ):
+                print(self.name, sold, close, self.scList.items())
+
+        self.sc = None
+        self.scList.clear()
+        if len(self.assigned) > self.hold:
+            j = [i for i in self.assigned if ( i < close)] if self.hold == 0 else [i for i in self.assigned if ( min(self.assigned) < i < close)]
+            l = 0
+            for i in j:
+                self.numSC += 1
+                self.scList[ close * (1 + (self.call_margin + l ) / 100) ].append(i)
+                l = l + 1
+
+            #if len(self.scList) > 0:
+                #print(self.name, "sell", self.assigned, j, close, self.scList)
+
+        if len(self.assigned) > self.maxPostion:
+            self.maxPostion = len(self.assigned)
+
+        self.sp = close * (1 - (self.put_margin if len(self.assigned) > 0 else self.put_margin-2) / 100)
+        self.numSP += 1
+        if self.max is None or self.max < row["high"]:
+            self.max = row["high"]
+
+        if self.min is None or self.min > row["low"]:
+            self.min = row["low"]
+
+        # self.balance += round(100 * 1/ (1+len(self.assigned)),2)
+        # print ("%s open %.2f close %0.2f position %d balance %0.2f "%(date, open, close,  self.position, self.stock + close *self.position))
+        # print (self.assigned, max(self.assigned) if len(self.assigned) > 0 else 0 )
+
+        self.positionList.append(self.position)
+        self.valueList.append((self.stock + row["close"] * self.position))
+
+
+class HoldNStrategy(Strategy):
+    def __init__(self, name, put_margin, call_margin, hold):
+        self.stock = 0.0
+        self.option = 0.0
+        self.position = 0
+        self.sp = None
+        self.sc = None
+        self.hold = hold
+        self.assigned = []
+        self.name = name + " Put " + str(put_margin) + "%" + " Call " + str(call_margin) + "% (min hold " + str(
+            hold) + ")"
+        self.num = 0
+        self.numSP = 0
+        self.numSC = 0
+        self.put_margin = put_margin
+        self.call_margin = call_margin
+        self.maxPostion = 0
+        self.max = None
+        self.min = None
+        self.positionList = []
+        self.valueList = []
+        self.scList = []
+
+    def Check(self, row):
+        close = row["close"]
+        open = row["open"]
+
+        if self.sp is not None:
+            if self.sp > close:
+                # print(self.name, "assingment buy", self.assigned, self.sp, close)
+                self.assigned.append(round(self.sp, 2))
+                self.position += 100
+                self.stock -= 100 * self.sp
+                # print ("Buy %f with %f"%(self.sp, close) )
+        if self.sc is not None:
+
+            if self.sc < close:
+                #print(self.name, "assignment sell", self.assigned, self.scList, close)
+                for i in self.scList:
+                    self.assigned.remove(i)
+                    self.position -= 100
+                    self.stock += 100 * self.sc
+
+                # print("Sold %f with %f" % (self.sc, close))
+
+        self.sc = None
+        self.scList = []
+        if len(self.assigned) > self.hold:
+            j = [i for i in self.assigned if ( i < close)] if self.hold == 0 else [i for i in self.assigned if ( min(self.assigned) < i < close)]
+            if len(j) > 0:
+                #print(self.name, "sell", self.assigned, j, close)
+                self.sc = close * (1 + self.call_margin / 100)
+                self.numSC += len(j)
+                self.scList = j
+
+        if len(self.assigned) > self.maxPostion:
+            self.maxPostion = len(self.assigned)
+
+        self.sp = close * (1 - (self.put_margin if len(self.assigned) > 0 else self.put_margin-2) / 100)
+        self.numSP += 1
+        if self.max is None or self.max < row["high"]:
+            self.max = row["high"]
+
+        if self.min is None or self.min > row["low"]:
+            self.min = row["low"]
+
+        # self.balance += round(100 * 1/ (1+len(self.assigned)),2)
+        # print ("%s open %.2f close %0.2f position %d balance %0.2f "%(date, open, close,  self.position, self.stock + close *self.position))
+        # print (self.assigned, max(self.assigned) if len(self.assigned) > 0 else 0 )
+
+        self.positionList.append(self.position)
+        self.valueList.append((self.stock + row["close"] * self.position))
+
+
+# actions = ["call", "put"]
+stocks = {"aapl": 3, "nflx": 1, "nvda": 1, "fb": 1, "msft": 2, "spy": 1, "low": 2}
+#stocks =  {"sq" : 1}
+
+# stocks =  {"nflx" : 1}
+ranges = {
+    "2019-2020": {
+        "start_date": "1/1/2019",
+        "end_date": "31/12/2020",
+        "first_monday": "7/1/2019",
+        "first_friday": "31/12/2019"}
+}
+
+for year, r in ranges.items():
+    for k, v in stocks.items():
+        print(k, r["start_date"], r["end_date"])
+        p = si.get_live_price(k)
+        data = si.get_data(k, r["start_date"])
+
+        strategy = []
+        strategy.append(HoldNStrategy(k, 5, 5, 1))
+        strategy.append(HoldNStrategy(k, 5, 5, 0))
+        strategy.append(HoldNStrategy(k, 5, 7, 1))
+        #strategy.append(DynamicStrategy(k, 5, 5, 1))
+        #strategy.append(DynamicStrategy(k, 5, 5, 0))
+        #strategy.append(DynamicStrategy(k, 5, 7, 1))
+        monday = dt.strptime(r["first_monday"], "%d/%m/%Y")
+        friday = dt.strptime(r["first_friday"], "%d/%m/%Y")
+        end = dt.strptime(r["end_date"], "%d/%m/%Y")
+
+        dateList = []
+        closeList = []
+
+        while monday < end:
+            range = data.loc[monday: friday]
+            # print (range)
+            row = {"date": monday.strftime("%d/%m/%Y"), "high": round(range["high"].max(), 2),
+                   "open": round(range.iloc[0]["open"], 2), "close": round(range.iloc[-1]["close"], 2),
+                   "low": round(range["low"].min(), 2)}
+            # print ( "Up" if (open  < close) else "Down" )
+            # upper = high - open
+            # body =  close - open
+            # lower = close - low
+
+            # print ("%s-%s, %.2f,%.2f, %.2f, %.2f"%(start.strftime("%d/%m/%Y"), end.strftime("%d/%m/%Y"), high, open, close, low))
+            for s in strategy:
+                s.Check(row)
+
+            dateList.append(row["date"])
+            closeList.append(row["close"])
+            monday += timedelta(7)
+            friday += timedelta(7)
+
+            # df = pd.DataFrame({'Date' : s.date,                   'Close': s.close})
+
+        fig, ax1 = plt.subplots()
+        # Define the date format
+        ax1.plot(dateList, closeList, label='close')
+
+        ax1.set_xlabel('Date')
+        ax1.set_ylabel('Price')
+
+        ax1.xaxis.set_major_locator(mdates.DayLocator(interval=5))
+        #ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+
+
+        ax2 = ax1.twinx()
+        for s in strategy:
+
+            print(
+                "   %s Position: %d Return (%.2f vs %.2f) , [sc %d sp %d], [max %d curr %d], best [min %.2f max %.2f]" %
+                (s.name, s.position, (row["close"] * s.position) + s.stock, (s.max - s.min) * 100, s.numSC,
+                 s.numSP, s.maxPostion, len(s.assigned), s.min, s.max))
+            print("   ", s.assigned)
+
+            ax2.xaxis.set_major_locator(mdates.DayLocator(interval=5))
+            #ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+            ax2.plot(dateList, s.positionList, label=s.name + ' position')
+            ax2.plot(dateList, s.valueList, label=s.name + ' value')
+        plt.legend(loc="upper left")
+
+        plt.title(k + " " + r["start_date"] + " - " + r["end_date"])
+        fig.autofmt_xdate()
+
+        #plt.xticks(rotation=90)
+        plt.show()
--- ib_trade.py
+++ ib_trade.py
@@ -8,60 +8,123 @@ import csv
 
 from collections import defaultdict
 from datetime import datetime, timedelta
+class RowParser:
+    header = []
+    def setheader(self, list):
+        self.header = list
+    def get(self, row, str):
+        return row[self.header.index(str)]
 
 trades = defaultdict(lambda:  defaultdict( lambda:  defaultdict( lambda:  defaultdict( float)) ))
 week = defaultdict(lambda:  defaultdict( lambda:  defaultdict( lambda:  defaultdict( float)) ))
+dividend = defaultdict( lambda:  defaultdict( float))
+withholdingTaxParsed = False
+def parseDividend(row):
+    stock = row['Description'].split('(')[0]
+    amount = float(row['Amount'])
+    date = row ['Date']
+    dividend[date][stock] += amount
+    
+
+def parseOption(row):
+    stock = row['Symbol'].split(' ')[0]
+    stock = '{num:04d}'.format(num=int(stock)) + '.HK' if row['Currency'] == 'HKD' else stock
+    proceed = row['Proceeds'] if row['Proceeds'].find('&nbsp') == -1 else row['Proceeds'][:(row['Proceeds'].find('&nbsp'))]
+    price = float(row['T. Price'] if row['T. Price'].find('&nbsp') == -1 else row['T. Price'][:(row['T. Price'].find('&nbsp'))])
+    amount = (float(proceed) + float(row['Comm/Fee']))
+    position = int(row['Quantity'].replace(',', ''))
+    #print('%s, %s, %.2f, %d, %.2f ' % (stock, row['Date'], amount, position, price))
+    #print(stock, row['Date'], amount, position, price)
+
+
 def parseTrade(row):
+    if row['Symbol'] == '9618.SPO':
+        return
     stock = row['Symbol'] if row['Asset Category'] == 'Stocks' else row['Symbol'].split(' ')[0]
-    stock = '{num:04d}'.format(num = int(stock))+'.HK' if row['Currency'] == 'HKD' else stock
+    if stock != '3690.SPO':
+        stock = '{num:04d}'.format(num = int(stock))+'.HK' if row['Currency'] == 'HKD'   else stock
     
     trade_date_time = datetime.strptime(row['Date/Time'], '%Y-%m-%d, %H:%M:%S')
-    trade_date_time_str = trade_date_time.strftime("%d/%m/%Y %H:%M:%S")
+    trade_date_time_str = trade_date_time.strftime("%m/%d/%Y")
     proceed = row['Proceeds'] if row['Proceeds'].find('&nbsp') == -1 else row['Proceeds'][:(row['Proceeds'].find('&nbsp'))]
     price = row['T. Price'] if row['T. Price'].find('&nbsp') == -1 else row['T. Price'][:(row['T. Price'].find('&nbsp'))]
     #print (row['Proceeds'], proceed)
     
     
     week_start = trade_date_time - timedelta(days=trade_date_time.weekday())
-    #print (trade_date_time.strftime("%d/%m/%Y" ), week_start.strftime("%d/%m/%Y"     ))
-    week_time_str = week_start.strftime("%d/%m/%Y")
-    
-    #print ("%s, %s, %s, %s, %s, %s, %s, %s"%(trade_date_time_str,row['Currency'],stock, row['Symbol'], row['Quantity'], price, proceed, row['Comm/Fee']))
-    
+    #print (trade_date_time.strftime("%d/%m/%Y" ), week_start.strftime("%d/%m/%Y"))
+    week_time_str = week_start.strftime("%m/%d/%Y")
+    if float(proceed) + float(row['Comm/Fee']) == 0:
+        #print ("skip %s, %s, %s, %s, %s, %s, %s, %s"%(trade_date_time_str,row['Currency'],stock, row['Symbol'], row['Quantity'], price, proceed, row['Comm/Fee']))
+        return
+
     #trades[stock]['Total']['amount'] += (float(proceed) + float(row['Comm/Fee']))
     #trades[stock]['Total']['position'] += int(row['Quantity'].replace(',', ''))
     week [week_time_str][row['Asset Category']][stock + ", " + row['Currency']]['amount'] += (float(proceed) + float(row['Comm/Fee']))
     week [week_time_str][row['Asset Category']][stock + ", " + row['Currency']]['position'] += int(row['Quantity'].replace(',', ''))
 
-    trades[row['Asset Category']][stock][trade_date_time_str+', '+row['Symbol']+', '+row['Currency']]['amount'] += (float(proceed) + float(row['Comm/Fee']))
-    trades[row['Asset Category']][stock][trade_date_time_str+', '+row['Symbol']+', '+row['Currency']]['position'] += int(row['Quantity'].replace(',', ''))
-    trades[row['Asset Category']][stock][trade_date_time_str+', '+row['Symbol']+', '+row['Currency']]['price'] += float(price.replace(',', ''))
-    
+    trades[row['Asset Category']][trade_date_time_str][row['Symbol']]['Symbol'] = stock
+    trades[row['Asset Category']][trade_date_time_str][row['Symbol']]['Currency'] = row['Currency']
+    trades[row['Asset Category']][trade_date_time_str][row['Symbol']]['amount'] += (float(proceed) + float(row['Comm/Fee']))
+    trades[row['Asset Category']][trade_date_time_str][row['Symbol']]['position'] += int(row['Quantity'].replace(',', ''))
+    trades[row['Asset Category']][trade_date_time_str][row['Symbol']]['price'] = float(price.replace(',', ''))
 
-skip = 0
-with open("C:/Users/u8009900/Downloads/U5668308_20200101_20201124.csv", 'r') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-    for row in spamreader:
-        if row[0] == 'Trades':
-            header = row
+#with open("/Users/rickyng/Downloads/U5797815_20210101_20210312.csv", 'r') as csvfile:
+
+
+with open("/Users/rickyng/Downloads/U5668308_20230102_20230628.csv", 'r') as csvfile:  #no1
+#with open("/Users/rickyng/Downloads/U9371321_20230102_20230526.csv", 'r') as csvfile:   #no2
+#with open("/Users/rickyng/Downloads/U5797815_20220103_20220113.csv", 'r') as csvfile:    #winnie
+    spam_reader = csv.reader(csvfile, delimiter=',')
+    header = []
+    for line in spam_reader:
+        if len(line) == 1:
+            continue
+        if line[1] == 'Header' or line[1] == 'Headers':
+            header = line
             #print (header)
-            trade_reader = csv.DictReader(csvfile, fieldnames=header)
-            for row in trade_reader:
-                if row['Trades'] != 'Trades':
-                    break
-                #if row['Asset Category'] == 'Equity and Index Options' and row['DataDiscriminator'] == 'Trade':
-                #if row['Asset Category'] == 'Stocks' and row['DataDiscriminator'] == 'Trade':
+        else:
+            row = dict(zip(header, line))
+            if line[0] == "Option Exercises, Assignments and Expirations":
+                if ( row['Type'] == 'Assignments' and row['Transaction Type'] != 'Assignment'):
+                    parseOption(row)
+            elif line[0] == 'Trades':
                 if (row['Asset Category'] == 'Stocks' or row['Asset Category'] == 'Equity and Index Options' ) and row['DataDiscriminator'] == 'Trade':
-                    if row['Header'] == 'Data':
-                        #date_time_obj = datetime. strptime(row['Date/Time'], '%d/%m/%y %H:%M:%S')
-                        parseTrade(row)
-                
-            
-    for c , j in trades.items():
-        for key, value in j.items():
+                    parseTrade(row)
+            elif line[0] == 'Cash Report':
+                #if (row['Currency']) == 'Base Currency Summary':
+                    print ("%s, %s, %0.2f"%(row['Currency Summary'], row['Currency'], float(row['Total'])))
+
+            elif line[0] == 'Withholding Tax':
+                parseDividend(row)
+                withholdingTaxParsed = True
+            elif not(withholdingTaxParsed) and line[0] == 'Dividends':
+                parseDividend(row)
+
+    with open('stock.csv', mode='w') as stock_file:
+        stock_writer = csv.writer(stock_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for key, value in trades['Stocks'].items():
             for k, v in value.items():
-                print ('%s, %s, %.2f, %.0f, %.2f '%(key, k, v['amount'], v['position'], v['price']))       
-                pass
+                # print ('%s, %s, %.2f, %.0f, %.2f '%(key, k, v['amount'], v['position'], v['price']))
+                stock_writer.writerow([key, k, v['Symbol'], v['Currency'], v['amount'], v['position'], v['price']])
+
+    with open('option.csv', mode='w') as option_file:
+        option_writer = csv.writer(option_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for key, value in trades['Equity and Index Options'].items():
+            for k, v in value.items():
+                # print ('%s, %s, %.2f, %.0f, %.2f '%(key, k, v['amount'], v['position'], v['price']))
+                option_writer.writerow([key, v['Symbol'],k,  v['Currency'], v['amount'], v['position'], v['price']])
+            
+    #for c , j in trades.items():
+    #print (dividend)
+    with open('dividend.csv', mode='w') as dividend_file:
+        dividend_writer = csv.writer(dividend_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for date, value in dividend.items():
+            for stock, amount in value.items():
+                dividend_writer.writerow([date, stock, amount])
+
+
                 
     for c , j in week.items():
         for key, value in j.items():
